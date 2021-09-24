6521b995572e380325a47728022c7adc78341611
Miguel22247 <minipachru@gmail.com>
2021-09-24 10:09:15 -0300

    add 1.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,15 @@
 $(document).ready(init);
+
+function init () {
+  const amenityObject = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObject[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObject[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenityObject);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+}
+}
