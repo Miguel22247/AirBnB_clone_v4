ca295bbce46593cb9676c7dc14353ed9b9d2396f
Miguel Pacheco <minipachru@gmail.com>
2021-09-26 13:21:58 -0300

    change python file

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,7 +34,7 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
